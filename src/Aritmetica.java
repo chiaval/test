7811b6d0d2b0f6d04adaf4bad831458ea19cca88
Federico Boaro <federicoboaro@Federicos-MBP.fritz.box>
2017-09-16 10:27:56 +0200

    Funz compare

---
+++ src/Aritmetica.java
@@ -0,0 +1,9 @@
+/**
+ * Created by federicoboaro on 07/11/2016.
+ */
+public class Aritmetica {
+
+    public int somma (int a, int b){
+        return (a+b);
+    }
+}
---
+++ src/Autodromo.java
@@ -0,0 +1,10 @@
+/**
+ * Created by federicoboaro on 07/11/2016.
+ */
+public class Autodromo {
+    public static void main (String args[]){
+        Auto fiatUno = new Auto();
+        fiatUno.cilindrata = 1000;
+        fiatUno.muoviti();
+    }
+}
